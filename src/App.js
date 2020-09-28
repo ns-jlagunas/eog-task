f4ae40eecb307d6ebec99d535febeccb6d41a33f
Angel Lagunas <jlagunas@nearsoft.com>
2020-09-28 10:16:49 -0600

    App component updated

---
+++ src/App.js
@@ -0,0 +1,55 @@
+import React from 'react';
+import createStore from './store';
+import { Provider } from 'react-redux';
+import { ToastContainer } from 'react-toastify';
+import { MuiThemeProvider, createMuiTheme } from '@material-ui/core/styles';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import 'react-toastify/dist/ReactToastify.css';
+import Wrapper from './components/Wrapper';
+import Chart from './components/Chart';
+import SelectMetric from './components/SelectMetric';
+
+const store = createStore();
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      main: 'rgb(39,49,66)',
+    },
+    secondary: {
+      main: 'rgb(197,208,222)',
+    },
+    background: {
+      default: 'rgb(226,231,238)',
+    },
+  },
+});
+
+const App = () => {
+  const [metrics, setMetrics] = React.useState([]);
+
+  const selectMetric = metricsArray => {
+    let preArray = metricsArray.filter(metric => metric.checked);
+    const minsAgo30 = new Date().getTime() - 1000 * 60 * 30;
+    preArray = preArray.map(metric => {
+      return {
+        after: minsAgo30,
+        metricName: metric.name,
+      };
+    });
+    setMetrics(preArray);
+  };
+  return (
+    <MuiThemeProvider theme={theme}>
+      <CssBaseline />
+      <Provider store={store}>
+        <Wrapper>
+          <SelectMetric onSelect={selectMetric}></SelectMetric>
+          <Chart metrics={metrics} />
+          <ToastContainer />
+        </Wrapper>
+      </Provider>
+    </MuiThemeProvider>
+  );
+};
+
+export default App;
